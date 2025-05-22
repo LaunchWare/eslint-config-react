50ef818bde62d069c82b62610a54e38cc71033e6
Dan Pickett <dan.pickett@launchware.com>
2025-05-22 12:30:06 -0400

    update eslint to suit recent preferences

--- src/index.ts
+++ src/index.ts
@@ -9,86 +9,96 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 })
 
-const flatConfig = tsEslint.config(
-  eslint.configs.recommended,
-  ...tsEslint.configs.strictTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true, // alternative: `project: ['./tsconfig.json']` for older setups
-      },
-    },
-  },
+export const getConfiguration = ({
+  tsconfigRootDirectory,
+  project,
+  allowDefaultProject,
+}: {
+  tsconfigRootDirectory: string
+  project?: string | boolean | string[] | null
+  allowDefaultProject?: string[]
+}) => {
+  const flatConfig = tsEslint.config(
+    eslint.configs.recommended,
+    //...tsEslint.configs.strictTypeChecked,
 
-  ...compat.extends("eslint-config-airbnb"),
+    ...compat.extends("eslint-config-airbnb"),
 
-  {
-    name: "@launchware/eslint-config-react/react",
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    plugins: {
-      react: (reactPlugin as any).default ?? reactPlugin,
-      "jsx-a11y": a11yPlugin,
-    },
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    rules: {
-      // General best practices
-      "no-shadow": "off",
-      "no-use-before-define": "off",
-      "no-underscore-dangle": "off",
-      semi: "off",
-
-      // TypeScript-specific
-      "@typescript-eslint/no-unused-vars": ["error"],
-
-      // React-specific
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
-      "react/jsx-props-no-spreading": "off",
-
-      // A11y
-      // (optional additions depending on your team’s commitment to accessibility)
-
-      // Import rules
-      "import/extensions": ["off"],
-      "import/order": [
-        "error",
-        {
-          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-          pathGroups: [
-            {
-              pattern: "react",
-              group: "external",
-              position: "before",
-            },
-          ],
-          pathGroupsExcludedImportTypes: ["react"],
-          "newlines-between": "always",
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
+    {
+      name: "@launchware/eslint-config-react/react",
+      files: ["src/**/*.ts", "src/**/*.tsx"],
+      languageOptions: {
+        globals: globals.browser,
+        parserOptions: {
+          tsconfigRootDir: tsconfigRootDirectory,
+          project,
+          ecmaFeatures: { jsx: true },
+          projectService: {
+            allowDefaultProject,
           },
         },
-      ],
-      "import/prefer-default-export": "off",
-      "import/no-cycle": "off",
-      "import/no-named-as-default": "off",
-      "import/no-named-as-default-member": "off",
+      },
+      plugins: {
+        react: (reactPlugin as any).default ?? reactPlugin,
+        "jsx-a11y": a11yPlugin,
+      },
+      rules: {
+        // General best practices
+        "no-shadow": "off",
+        "no-use-before-define": "off",
+        "no-underscore-dangle": "off",
+        semi: "off",
+
+        // TypeScript-specific
+        //"@typescript-eslint/no-unused-vars": ["error"],
+
+        // React-specific
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
+        "react/jsx-props-no-spreading": "off",
+
+        // A11y
+        // (optional additions depending on your team’s commitment to accessibility)
+
+        // Import rules
+        "import/extensions": ["off"],
+        "import/order": [
+          "error",
+          {
+            groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+            pathGroups: [
+              {
+                pattern: "react",
+                group: "external",
+                position: "before",
+              },
+            ],
+            pathGroupsExcludedImportTypes: ["react"],
+            "newlines-between": "always",
+            alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+            },
+          },
+        ],
+        "import/prefer-default-export": "off",
+        "import/no-cycle": "off",
+        "import/no-named-as-default": "off",
+        "import/no-named-as-default-member": "off",
+      },
+    }
+  )
+
+  const config = {
+    meta: {
+      name: "@launchware/eslint-config-react",
+      version: "0.2.0",
+    },
+    configs: {
+      recommended: flatConfig,
     },
   }
-)
 
-export default {
-  meta: {
-    name: "@launchware/eslint-config-react",
-    version: "0.1.3",
-  },
-  configs: {
-    recommended: flatConfig,
-  },
+  return config
 }
